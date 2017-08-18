f8a0af7702852897d074c82c144461ff548dace7
Zeke Sikelianos <zeke@sikelianos.com>
2017-08-17 18:42:31 -0700

    refactor the demo

--- demo.js
+++ demo.js
@@ -2,11 +2,9 @@ const yo = require('yo-yo')
 const path = require('path')
 const pick = require('.')
 
-const results = require('electron-apps')
+const palettes = require('electron-apps')
   .slice(26)
   .map(app => {
-    console.clear()
-    console.log(app)
     const {iconColors, slug, icon128} = app
     return {
       colors: iconColors,
@@ -17,44 +15,45 @@ const results = require('electron-apps')
     }
   })
 
-function list (results) {
-  return yo`<ul class="palettes">
-    ${results.map(function (result) {
-      const {colors, icon, goodColorOnBlack, goodColorOnWhite, name} = result
-      return yo`<li>
-        
+function render (palettes) {
+  return yo`<content>
+
+    <ul class="palettes heading">
+      <li>
         <ul class="swatches">
-          ${colors.map(function (color) {
-            return yo`<li style="background:${color}">${color}</li>`
-          })}
-          <li class="picked" style="background:${goodColorOnBlack};color:black;">${goodColorOnBlack}</li>
-          <li class="picked" style="background:${goodColorOnWhite}">${goodColorOnWhite}</li>
-          <li class="icon"><img src="${icon}">${name}</li>
+          <li>Palette</li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li>On Black</li>
+          <li></li>
+          <li>On White</li>
+          <li></li>
+          <li>Icon</li>
         </ul>
-      </li>`
-    })}
-  </ul>`
+      </li>
+    </ul>
+
+    <ul class="palettes">
+      ${palettes.map(renderPalette)}
+    </ul>
+  </content>`
 }
 
-function key () {
-  return yo`<ul class="palettes heading">
-    <li>
-      <ul class="swatches">
-        <li>Palette</li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li>On Black</li>
-        <li></li>
-        <li>On White</li>
-        <li></li>
-        <li>Icon</li>
-      </ul>
-    </li>
-  </ul>`
+function renderPalette (palette) {
+  const {colors, icon, goodColorOnBlack, goodColorOnWhite, name} = palette
+  return yo`<li>
+    <ul class="swatches">
+      ${colors.map(color => {
+        return yo`<li style="background:${color}">${color}</li>`
+      })}
+      <li class="picked" style="background:${goodColorOnBlack};color:black;">${goodColorOnBlack}</li>
+      <li class="picked" style="background:${goodColorOnWhite}">${goodColorOnWhite}</li>
+      <li class="icon"><img src="${icon}">${name}</li>
+    </ul>
+  </li>`
 }
 
-document.body.appendChild(key())
-document.body.appendChild(list(results))
+document.body.appendChild(render(palettes))
